7eb7313e9e29fa23afd5328abb01eea297d211c3
AnthonyBuncio <anthonyb.ft@gmail.com>
2017-02-23 09:28:22 -0600

    added search function

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
-console.log('do we have jquery?')
-console.log($)
-
 //**** PROMISES && CONTAINER NODES ****
-var profilePromise = $.getJSON('https://api.github.com/users/AnthonyBuncio')
-var projectPromise = $.getJSON('https://api.github.com/users/AnthonyBuncio/repos')
-var leftNode = document.querySelector('#left') 
-var rightNode = document.querySelector('#right')
+var myProfile = 'https://api.github.com/users/AnthonyBuncio',
+	myProject = 'https://api.github.com/users/AnthonyBuncio/repos',
+	userProfile = 'https://api.github.com/users/', //we can use this for user project and add '/repos' to the end
+	leftNode = document.querySelector('.left'), 
+	rightNode = document.querySelector('#right'),
+	inputSearch = document.querySelector('.searchBar')
 
 //takes in userObj which already has properties we can grab
 //we can add HTML by selecting certain properties of that obj
@@ -17,10 +16,10 @@ var getUser = function(userObj) {
 	getString += '<img src="' + userObj.avatar_url + '"/>' //get image
 	getString += '<h1>' + userObj.name + '</h1>' //get name
 	getString += '<h2>' + userObj.bio + '</h2>' //get bio
-	getString += '' //line seperator
+	getString += '<hr>' //line seperator
 	getString += '<p>' + userObj.location + '</p>' //get location
-	getString += '<p>' + userObj.email + '<p>' //get email
-	getString += '<p>' + userObj.blog + '</p>' //get website
+	getString += '<a class="email" href="mailto:' + userObj.email + '">' + userObj.email + '</a><br><br>' //get email
+	getString += '<a class="blog" href="' + userObj.blog + '">' + userObj.blog + '</a>' //get website
 	getString += '</div>'
 	leftNode.innerHTML = getString
 }
@@ -32,7 +31,7 @@ var getUser = function(userObj) {
 var getProjectInfo = function(singleObj) {
 	var eachObjHTML = ''
 	eachObjHTML += '<li>' + '<a class="repo-link" href=' + singleObj.html_url + '>' + singleObj.name + '</a>'
-	eachObjHTML += '<p>' + singleObj.language + '</p>' + '</li><hr>'
+	eachObjHTML += '<p class="language">' + singleObj.language + '</p>' + '</li><hr>'
 	return eachObjHTML
 	console.log(eachObjHTML)
 }
@@ -48,6 +47,40 @@ var getRepo = function(projArray) {
 }
 
 
+// *** IGNORE *** i made a single function of this below
+// var makeUserRequest = function(url) {
+// 	var gitPromise = $.getJSON(url)
+// 	gitPromise.then(getUser)
+// }
 
-profilePromise.then(getUser)
-projectPromise.then(getRepo)
+// var makeRepoRequest = function(url) {
+// 	var gitPromise = $.getJSON(url)
+// 	gitPromise.then(getRepo)
+// }
+
+// makeUserRequest(myProfile)
+// makeRepoRequest(myProject)
+
+
+//create a single function to get both user and repo information
+//this is a shorter version of the above function
+var makeRequest = function(url1, url2) {
+	var gitPromise1 = $.getJSON(url1)
+	gitPromise1.then(getUser)
+	var gitPromise2 = $.getJSON(url2)
+	gitPromise2.then(getRepo)
+}
+//this function is invoked as the page shown on load each time
+//until search bar contains value and enter key is pressed
+makeRequest(myProfile, myProject)
+
+//listens for 'enter' keydown and adds search bar value to url
+//receives user info and repos to show in dom
+inputSearch.addEventListener('keydown', function(eventObj) {
+	if (eventObj.keyCode === 13) {
+		var getUser1 = userProfile + eventObj.target.value
+		var getUser2 = userProfile + eventObj.target.value + '/repos'
+		makeRequest(getUser1, getUser2)
+		eventObj.target.value = ''
+	}
+})
